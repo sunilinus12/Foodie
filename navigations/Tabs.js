7611cbb5ab6f9ebf8678faea0b5f73cd50e4452a
sunilinus12 <www.suniljaunwal2@gmail.com>
2022-10-29 14:35:06 +0530

    bottom navigator functionality done

--- navigations/Tabs.js
+++ navigations/Tabs.js
@@ -1,9 +1,8 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from '../screens/Home';
-import OrderDelivery from '../screens/OrderDelivery';
-import Restaurant from '../screens/Restaurant';
+import { Svg, Path } from 'react-native-svg';
 import { COLORS, icons } from "../constants";
 export default function Tabs() {
     const Tab = createBottomTabNavigator();
@@ -11,8 +10,14 @@ export default function Tabs() {
         <Tab.Navigator
             screenOptions={{
                 headerShown: false,
-                tabBarShowLabel: false
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    elevation: 0,
+                    backgroundColor: "transparent",
+                    borderTopWidth: 0
+                }
             }}
+
         >
             <Tab.Screen name="Home" component={Home}
 
@@ -26,6 +31,12 @@ export default function Tabs() {
                                 tintColor: focused ? COLORS.primary : COLORS.secondary
                             }}
                             resizeMode="contain"
+                        />
+                    ),
+
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
                         />
                     )
                 }}
@@ -45,6 +56,11 @@ export default function Tabs() {
                             }}
                         />
                     ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
 
                 }}
             />
@@ -64,6 +80,11 @@ export default function Tabs() {
                             }}
                         />
                     ),
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
 
                 }}
             />
@@ -84,6 +105,12 @@ export default function Tabs() {
                         />
                     ),
 
+                    tabBarButton: (props) => (
+                        <TabBarCustomButton
+                            {...props}
+                        />
+                    )
+
                 }}
             />
 
@@ -91,4 +118,59 @@ export default function Tabs() {
         </Tab.Navigator>
     )
 
+}
+
+const TabBarCustomButton = ({ accessibilityState, children, onPress }) => {
+
+    var isSelected = accessibilityState.selected
+
+    if (isSelected) {
+        return (
+            <View style={{ flex: 1, alignItems: "center" }}>
+                <View style={{ flexDirection: 'row', position: 'absolute', top: 0 }}>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                    <Svg
+                        width={70}
+                        height={61}
+                        viewBox="0 0 75 61"
+                    >
+                        <Path
+                            d="M75.2 0v61H0V0c4.1 0 7.4 3.1 7.9 7.1C10 21.7 22.5 33 37.7 33c15.2 0 27.7-11.3 29.7-25.9.5-4 3.9-7.1 7.9-7.1h-.1z"
+                            fill={COLORS.white}
+                        />
+                    </Svg>
+                    <View style={{ flex: 1, backgroundColor: COLORS.white }}></View>
+                </View>
+
+                <TouchableOpacity
+                    style={{
+                        top: -22.5,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: 50,
+                        height: 50,
+                        borderRadius: 25,
+                        backgroundColor: COLORS.white
+                    }}
+                    onPress={onPress}
+                >
+                    {children}
+                </TouchableOpacity>
+            </View>
+        )
+    } else {
+        return (
+            <TouchableOpacity
+                style={{
+                    flex: 1,
+                    height: 60,
+                    backgroundColor: COLORS.white
+                }}
+                activeOpacity={1}
+                onPress={onPress}
+            >
+                {children}
+            </TouchableOpacity>
+        )
+    }
 }
